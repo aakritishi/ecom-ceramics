038fde62029c9ebed6f804f79988318e9e4b905d
aakriti <aakritip512@gmail.com>
2024-09-01 16:07:51 +0545

    initial commit from branch

--- src/components/home/IntroductionSection.js
+++ src/components/home/IntroductionSection.js
@@ -5,18 +5,20 @@ const IntroductionSection = () => {
   return (
     <div className="flex flex-col md:flex-row bg-white">
       {/* Left Section */}
-      <div className="md:w-1/2 p-8 bg-[#582707] text-white flex flex-col justify-center z-50">
-        <h1 className="text-4xl md:text-5xl font-bold mb-4">
-          Crafted with Care <br /> for Memorable Moments.
-        </h1>
-        <p className="mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-        </p>
-        <div className=' flex items-center justify-center'>
-        <button className="bg-white text-[#582707] py-2 px-6 rounded-full hover:bg-gray-200 transition w-1/3 font-semibold">
-          SHOP NOW
-        </button>
-        </div>   
+      <div className="md:w-1/2 bg-[#582707] text-white flex flex-col justify-center z-50">
+        <div className='w-[94%] mx-auto py-3'>
+            <h1 className="text-4xl md:text-5xl font-bold mb-4">
+            Crafted with Care <br /> for Memorable Moments.
+            </h1>
+            <p className="mb-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+            </p>
+            <div className=' flex items-center justify-center'>
+            <button className="bg-white text-[#582707] py-2 px-6 rounded-full hover:bg-gray-200 transition w-1/3 font-semibold">
+            SHOP NOW
+            </button>
+            </div>  
+        </div> 
       </div>
 
       {/* Right Section */}
